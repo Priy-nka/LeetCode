723fee7640641de923783bd5178ad6ea2971e218
Priyanka <97823620+Priy-nka@users.noreply.github.com>
2024-03-04 23:20:18 +0530

    Time: 4 ms (74.87%), Space: 13.1 MB (23.53%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.cpp
+++ 0948-bag-of-tokens/0948-bag-of-tokens.cpp
@@ -3,28 +3,7 @@ public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
         int score=0,len=tokens.size();
         sort(tokens.begin(),tokens.end());
-        // for(int i=0;i<len;i++)
-        // {
-        //     if(power>=tokens[i])
-        //     {
-        //         score++;
-        //         power=power-tokens[i];
-        //     }
-        //     else if (score>=1)
-        //     {
-        //         power=power+tokens[i];
-        //         score--;
-        //     }
-        // }
         int low=0, high=len-1;
-        // if(tokens[low]<=power)
-        // {
-        //     score++;
-        //     power=power-tokens[low];
-        //     low++;
-        // }
-        // else
-        //     return 0;
          if(len<=2)
          {
              if (len==0)
