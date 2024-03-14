564f6f8607cb27099f6166b4fd13e46a1655a39c
Priyanka <97823620+Priy-nka@users.noreply.github.com>
2024-03-14 22:17:38 +0530

    Time: 2298 ms (5.02%), Space: 31.2 MB (52.67%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,20 +1,58 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int ans=0, len=nums.size();
-        for (int i = 0; i < len-1; i++)
+        // int ans=0, len=nums.size();
+        // for (int i = 0; i < len-1; i++)
+        // {
+        //     int sum=nums[i];
+        //     if (nums[i]==goal)
+        //         ans++;
+        //     for (int j=i+1; j<len; j++)
+        //     {
+        //         sum+=nums[j];
+        //         if (sum == goal)
+        //         {
+        //             ans++;
+        //         }
+        //     }
+        // }
+        // if (nums[len-1]==goal)
+        //     ans++;
+        // return ans;
+        int len=nums.size(), ans=0;
+        if (len==1)
         {
-            int sum=nums[i];
-            if (nums[i]==goal)
+            if (nums[0]==goal)
+                return 1;
+            return 0;
+        }
+        int l=0, r=1;
+        long long sum=nums[0];
+        while (l<len-1)
+        {
+            if(nums[l]==goal)
                 ans++;
-            for (int j=i+1; j<len; j++)
+            while (r<len)
             {
-                sum+=nums[j];
-                if (sum == goal)
-                {
-                    ans++;
-                }
+                sum+=nums[r];
+            if (sum==goal)
+            {
+                ans++;
+                r++;
             }
+            else if (sum>goal)
+            {
+                r=len;
+            }
+            else
+            {
+                r++;
+            } 
+            //sum+=nums[r];
+            }
+            l++;
+            r=l+1;
+            sum=nums[l];
         }
         if (nums[len-1]==goal)
             ans++;
