3fc95c8e00ffa63f585babd29978d583b0503567
Priyanka <97823620+Priy-nka@users.noreply.github.com>
2024-10-02 11:50:27 +0530

    Time: 107 ms (21.61%), Space: 45.1 MB (24.24%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        map<int, int> org_mp;
         vector<int> temp=arr;
         int n=arr.size();
-        for (int i=0; i<n; i++)
-        {
-            org_mp[i]=arr[i];
-        }
         sort(arr.begin(), arr.end());
         vector<int> rank;
         map<int, int>rank_mp;
@@ -25,10 +20,6 @@ public:
             int curr=rank_mp[temp[i]];
             rank.push_back(curr);
         }
-        // for (auto m:rank_mp)
-        // {
-        //     cout<<m.first<<" "<<m.second<<endl;
-        // }
         
         return rank;
     }
