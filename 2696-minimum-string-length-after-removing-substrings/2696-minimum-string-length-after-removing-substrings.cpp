ae8d17e4d6c16e923b6ad4ef81460f1dcb8b5d70
Priyanka <97823620+Priy-nka@users.noreply.github.com>
2024-10-07 12:27:06 +0530

    Time: 15 ms (8.14%), Space: 11.7 MB (18.56%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -4,21 +4,18 @@ public:
         string ab="AB"; string cd="CD";
         stack<char> st;
         st.push(s[0]);
-        //cout<<(char)st.top()<<endl;
         for (int i=1; i<s.size(); i++)
         {
             if (!st.empty())
             {
-            string curr = st.top()+string(1, s[i]);
-            //cout<<curr<<" ";
-            if ((curr==ab) || (curr==cd))
-                st.pop();
-            else
-                st.push(s[i]);
+                string curr = st.top()+string(1, s[i]);
+                if ((curr==ab) || (curr==cd))
+                    st.pop();
+                else
+                    st.push(s[i]);
             }
             else
                 st.push(s[i]);
-            //cout<<st.top()<<endl;
         }
         return st.size();
     }
