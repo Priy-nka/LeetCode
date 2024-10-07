239d8753cc376deebdaea80010014ea0699f6a49
Priyanka <97823620+Priy-nka@users.noreply.github.com>
2024-10-07 13:04:55 +0530

    Time: 14 ms (8.35%), Space: 10.8 MB (78.76%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -2,26 +2,19 @@ class Solution {
 public:
     int minLength(string s) {
         int n = s.size();
-        // s+="LLLL";
         for(int i=0;i<s.size()-1;i++){
             if(i==n) break;
             string temp = "";
             temp += s[i];
             temp += s[i+1];
-            cout<<temp<<endl;
             if(temp == "AB" || temp=="CD"){
                 auto start  = s.begin()+i;
                 auto end  = s.begin()+i+2;
-                // cout<<i<<" : "<<s<<endl;
                 s.erase(start,end);
-                cout<<i<<" : "<<s<<endl;
                 if(i>0) i=i-2;
                 else i = -1;
-
-                // i=i-2;
             }
         }
-        // if(s == "AB" || s=="CD") return 0;
 
         return s.size();
     }
